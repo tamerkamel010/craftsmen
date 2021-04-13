cca99247b3aecb730ab77ab503114cb0a1949e1b
SalahElsayed <salahelsayed995@gmail.com>
2021-04-13 15:48:37 +0200

    add admin page

--- job/admin.py
+++ job/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Client, Job, Category 
 
-# Register your models here.
+admin.site.register(Client)
+admin.site.register(Job)
+admin.site.register(Category)
